bd7c5ef8d07fbb10eded64fedd45471cf4a69486
gajera avadh <gajeraavadh0@gmail.com>
2022-11-10 21:03:17 +0530

    Create c++.c

---
+++ C PROGRAMMING/CLASS WORK/c++.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main()
+{
+	
+	char name[15]="avadh\tgajera";
+    int age=20;
+    float salary=25000.536324;
+
+    printf("name\t:%s\n",name);
+    printf("age\t:%d\n",age);
+    printf("salary\t:%.3f",salary);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
