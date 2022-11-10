b5f73a289479224caa4bf24df5a91211dd880d7b
gajera avadh <gajeraavadh0@gmail.com>
2022-11-10 20:59:10 +0530

    Create AVADH INFORMATION.c

---
+++ C PROGRAMMING/LAB WORK/AVADH INFORMATION.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("name\t:\tavadh gajera\n");
+	printf("d.o.b\t:\t25/09/2002\n");
+	printf("gender\t:\tmale\n");
+	printf("address\t:\tamreli\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
