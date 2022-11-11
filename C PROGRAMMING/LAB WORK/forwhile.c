af6dbba5781d2899606b9f070dc73fff672ddd99
gajera avadh <gajeraavadh0@gmail.com>
2022-11-11 08:28:49 +0530

    Update forwhile.c

--- C PROGRAMMING/LAB WORK/forwhile.c
+++ C PROGRAMMING/LAB WORK/forwhile.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<stdio.h>
 
 void main()
@@ -8,4 +9,16 @@ void main()
 	{
 	 printf("%d\t",i);
 	}
+=======
+#include<stdio.h>
+
+void main()
+
+{
+	int i;
+	for(i=100;i>=1;i--)
+	{
+	 printf("%d\t",i);
+	}
+>>>>>>> c6edcc72124eef42a90e3d57adcc3b9097bb6d01
 }
