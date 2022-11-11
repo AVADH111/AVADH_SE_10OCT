a8590c9f7db291b4f615401d10f7695a5208e52e
gajera avadh <gajeraavadh0@gmail.com>
2022-11-11 08:28:44 +0530

    Update whiledemo.c

--- C PROGRAMMING/LAB WORK/whiledemo.c
+++ C PROGRAMMING/LAB WORK/whiledemo.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<stdio.h>
 
 void main()
@@ -10,4 +11,18 @@ void main()
 		printf("%d,avadh",i);
 		i++;
 	}
+=======
+#include<stdio.h>
+
+void main()
+{
+	int i;
+	
+	while(i=1)
+	
+	{
+		printf("%d,avadh",i);
+		i++;
+	}
+>>>>>>> c6edcc72124eef42a90e3d57adcc3b9097bb6d01
 }
