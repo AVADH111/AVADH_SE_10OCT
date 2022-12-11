03cc01877fbba3983461d9541a121a0e306a981b
AVADH111 <115629700+AVADH111@users.noreply.github.com>
2022-12-11 20:51:45 +0530

    Add files via upload

---
+++ C PROGRAMMING/ASSIGEMENT/moduel . 3.1/CALC.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void main()
+{
+	
+	int a,b;
+	
+	printf("\nenter a :");
+	scanf("%d",&a);
+	printf("enter b :");
+	scanf("%d",&b);
+	printf("\n a=%d ,b=%d",a,b);
+	
+	printf("\naddtion:%d",(a+b));
+	printf("\nsubtraction:%d",(a-b));
+	printf("\nmuitiplication:%d",(a*b));
+	printf("\ndivison:%f",(a/(float)b));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
