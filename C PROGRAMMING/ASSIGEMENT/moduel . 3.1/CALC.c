0bd2299d149681c71c0f3462ce4ca553089059a7
gajera avadh <gajeraavadh0@gmail.com>
2022-12-11 20:58:43 +0530

    Update CALC.c

--- C PROGRAMMING/ASSIGEMENT/moduel . 3.1/CALC.c
+++ C PROGRAMMING/ASSIGEMENT/moduel . 3.1/CALC.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<stdio.h>
 
 void main()
@@ -35,3 +36,42 @@ void main()
 
 
 
+=======
+#include<stdio.h>
+
+void main()
+{
+	
+	int a,b;
+	
+	printf("\nenter a :");
+	scanf("%d",&a);
+	printf("enter b :");
+	scanf("%d",&b);
+	printf("\n a=%d ,b=%d",a,b);
+	
+	printf("\naddtion:%d",(a+b));
+	printf("\nsubtraction:%d",(a-b));
+	printf("\nmuitiplication:%d",(a*b));
+	printf("\ndivison:%f",(a/(float)b));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+>>>>>>> 03cc01877fbba3983461d9541a121a0e306a981b
