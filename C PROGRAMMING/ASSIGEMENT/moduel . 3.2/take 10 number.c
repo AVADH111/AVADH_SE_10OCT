3be345d0286f9afba8b568deb973413a9d1d08d7
AVADH111 <115629700+AVADH111@users.noreply.github.com>
2022-12-14 08:25:16 +0530

    Add files via upload

---
+++ C PROGRAMMING/ASSIGEMENT/moduel . 3.2/take 10 number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+int i,j;
+
+printf(“enter the numbers”);
+
+scanf(“%d”,&j);
+
+for(i=1;i<=j;i++)
+
+{
+
+printf(“the numbers are=%d”,i);
+
+}
+
+getch();
+
+}
