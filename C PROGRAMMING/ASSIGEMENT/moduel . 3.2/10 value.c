c6c648c8a92a15f066977cc4852ca80e4f1387f0
AVADH111 <115629700+AVADH111@users.noreply.github.com>
2022-12-16 07:41:06 +0530

    Add files via upload

---
+++ C PROGRAMMING/ASSIGEMENT/moduel . 3.2/10 value.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+ 
+int main() 
+{ 
+    int arr[10], i; 
+    
+    printf("Enter 10 values\n"); 
+    for(i = 0; i < 10; i++){ 
+        scanf("%d",&arr[i]); 
+    } 
+    for(i = 0; i < 10; i++){ 
+        
+    } 
+    
+ 
+    return 0; 
+} 
---
+++ C PROGRAMMING/ASSIGEMENT/moduel . 3.2/WAP CALC.c
@@ -0,0 +1,77 @@
+<<<<<<< HEAD
+#include<stdio.h>
+
+void main()
+{
+	
+	int a,b;
+	
+	printf("\nenter a :");
+	scanf("%d",&a);
+	printf("enter b :");
+	scanf("%d",&b);
+	printf("\n a=%d ,b=%d",a,b);
+	
+	printf("\naddtion:%d",(a+b));
+	printf("\nsubtraction:%d",(a-b));
+	printf("\nmuitiplication:%d",(a*b));
+	printf("\ndivison:%f",(a/(float)b));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+=======
+#include<stdio.h>
+
+void main()
+{
+	
+	int a,b;
+	
+	printf("\nenter a :");
+	scanf("%d",&a);
+	printf("enter b :");
+	scanf("%d",&b);
+	printf("\n a=%d ,b=%d",a,b);
+	
+	printf("\naddtion:%d",(a+b));
+	printf("\nsubtraction:%d",(a-b));
+	printf("\nmuitiplication:%d",(a*b));
+	printf("\ndivison:%f",(a/(float)b));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+>>>>>>> 03cc01877fbba3983461d9541a121a0e306a981b
